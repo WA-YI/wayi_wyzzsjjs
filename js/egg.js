8ee3e33b836ab9805bf384ef621f48176d27725a
娃伊先森 <abc980514@qq.com>
2017-12-20 00:47:08 +0800

    2017-12-20-01

--- js/egg.js
+++ js/egg.js
@@ -2,7 +2,12 @@
 
 function xieyi()
 {
-	alert("根本没有什么协议好吧？！")
+	var msg = "根本没有什么协议OK吗"
+	if (confirm(msg)==true){
+		return alert("要交代的几个事情：\n1．这个网站里所有链接都是可用的\n2．目录并未收录所有Disney游戏\n3．除图片、字体素材外其余文件均为本人版权所有\n4．千万别做程序猿（易脱发猝死\n5．别的我辶");
+	} else {
+			return alert("わい爱你呦！o(*^＠^*)o");
+		}
 }
 
 function sousuo()
---
+++ js/filter.js
@@ -0,0 +1,89 @@
+// JavaScript Document
+
+function open()
+{
+	if (document.getElementById('cate').style.display=="none")
+	{ 
+		document.getElementById("filter").style.color="#222";
+		document.getElementById("cate").style.display="block";
+	} else {
+		document.getElementById("filter").style.color="#bbb";
+		document.getElementById("cate").style.display="none";
+	}
+}
+
+function all()
+{
+	document.getElementById("game01").style.display="block";
+	document.getElementById("game02").style.display="block";
+	document.getElementById("game03").style.display="block";
+	document.getElementById("game04").style.display="block";
+	document.getElementById("game05").style.display="block";
+	document.getElementById("game06").style.display="block";
+	document.getElementById("game07").style.display="block";
+	document.getElementById("game08").style.display="block";
+	document.getElementById("game09").style.display="block";
+	document.getElementById("game10").style.display="block";
+	document.getElementById("game11").style.display="block";
+	document.getElementById("game12").style.display="block";
+	document.getElementById("game13").style.display="block";
+	document.getElementById("game14").style.display="block";
+	document.getElementById("game15").style.display="block";
+}
+
+function mickey()
+{
+	document.getElementById("game01").style.display="none";
+	document.getElementById("game02").style.display="none";
+	document.getElementById("game03").style.display="none";
+	document.getElementById("game04").style.display="none";
+	document.getElementById("game05").style.display="none";
+	document.getElementById("game06").style.display="none";
+	document.getElementById("game07").style.display="none";
+	document.getElementById("game08").style.display="none";
+	document.getElementById("game09").style.display="none";
+	document.getElementById("game10").style.display="block";
+	document.getElementById("game11").style.display="block";
+	document.getElementById("game12").style.display="none";
+	document.getElementById("game13").style.display="block";
+	document.getElementById("game14").style.display="none";
+	document.getElementById("game15").style.display="block";
+}
+
+function net()
+{
+	document.getElementById("game01").style.display="none";
+	document.getElementById("game02").style.display="none";
+	document.getElementById("game03").style.display="none";
+	document.getElementById("game04").style.display="none";
+	document.getElementById("game05").style.display="none";
+	document.getElementById("game06").style.display="none";
+	document.getElementById("game07").style.display="none";
+	document.getElementById("game08").style.display="none";
+	document.getElementById("game09").style.display="none";
+	document.getElementById("game10").style.display="none";
+	document.getElementById("game11").style.display="block";
+	document.getElementById("game12").style.display="none";
+	document.getElementById("game13").style.display="block";
+	document.getElementById("game14").style.display="block";
+	document.getElementById("game15").style.display="none";
+}
+
+function ap()
+{
+	document.getElementById("game01").style.display="block";
+	document.getElementById("game02").style.display="block";
+	document.getElementById("game03").style.display="none";
+	document.getElementById("game04").style.display="none";
+	document.getElementById("game05").style.display="none";
+	document.getElementById("game06").style.display="block";
+	document.getElementById("game07").style.display="none";
+	document.getElementById("game08").style.display="none";
+	document.getElementById("game09").style.display="none";
+	document.getElementById("game10").style.display="none";
+	document.getElementById("game11").style.display="none";
+	document.getElementById("game12").style.display="none";
+	document.getElementById("game13").style.display="none";
+	document.getElementById("game14").style.display="none";
+	document.getElementById("game15").style.display="none";
+}
