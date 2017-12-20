1827ade4c9901d2e8c88831ca2136fb48668ccb2
娃伊先森 <abc980514@qq.com>
2017-12-20 18:06:38 +0800

    尝试适配手机端Hover

---
+++ js/hover.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+
+var myLinks = document.getElementsByTagName('a');
+for(var i = 0; i < myLinks.length; i++){
+　　myLinks[i].addEventListener(’touchstart’, function(){this.className = "hover";}, false);
+　　myLinks[i].addEventListener(’touchend’, function(){this.className = "";}, false);
+}
+
+//源码来自：http://www.qdfuns.com/notes/17610/1f8dd1bda8297020624639f6d6227ea3.html
